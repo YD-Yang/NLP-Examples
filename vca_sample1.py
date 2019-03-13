42ec1d381e5b39d85bbb9795f17fb02c4dc8f9a5
Yandan-Yang <48411346+Yandan-Yang@users.noreply.github.com>
2019-03-12 23:49:01 -0500

    Add files via upload

---
+++ vca_sample1.py
@@ -0,0 +1,156 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  1 11:24:53 2017
+
+@author: yyang
+"""
+
+import pandas as pd
+import os
+import re
+
+import nltk
+from nltk import word_tokenize
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.stem.porter import *
+
+from gensim.corpora.dictionary import Dictionary
+from gensim.models.tfidfmodel import TfidfModel
+from gensim.models.ldamodel import LdaModel
+
+import json
+import pyLDAvis.gensim
+
+
+sns.set(style="ticks")
+seed=42
+np.random.seed(seed)
+pd.options.display.max_columns = 20
+
+dir_path = os.path.realpath('')
+path = os.path.join(dir_path, 'data\\VCA_Training_2.csv')
+vca_s1 = pd.read_csv(path, header=0, index_col=0)
+print("Dataset has {} rows, {} columns.".format(*vca_s1.shape))
+
+corpus = vca_s1["text"]
+
+def preprocess_text(corpus):
+    """Takes a corpus in list format and applies basic preprocessing steps of word tokenization,
+     removing of english stop words, lower case and lemmatization."""
+    processed_corpus = []
+    english_words = set(nltk.corpus.words.words())
+    english_stopwords = set(stopwords.words('english'))
+    wordnet_lemmatizer = WordNetLemmatizer()
+    stemmer = PorterStemmer()
+    tokenizer = RegexpTokenizer(r'[\w|!]+')
+    for row in corpus:
+        word_tokens = tokenizer.tokenize(row)
+        word_tokens_lower = [t.lower() for t in word_tokens]
+        word_tokens_lower_english = [t for t in word_tokens_lower if t in english_words or not t.isalpha()]
+        word_tokens_no_stops = [t for t in word_tokens_lower_english if not t in english_stopwords]
+        word_tokens_no_stops_lemmatized = [wordnet_lemmatizer.lemmatize(t) for t in word_tokens_no_stops]
+        word_tokens_no_stops_lemmatized_stem = [stemmer.stem(t) for t in word_tokens_no_stops_lemmatized]
+        processed_corpus.append(word_tokens_no_stops_lemmatized_stem)
+    return processed_corpus
+
+def nlp_model_pipeline(processed_corpus):
+    """Takes processed corpus and produce dictionary, doc_term_matrix and LDA model"""
+    # Creates the term dictionary (every unique term in corpus is assigned an index)
+    dictionary = Dictionary(processed_corpus)
+    # Convert corpus into Document Term Matrix using dictionary prepared above
+    doc_term_matrix = [dictionary.doc2bow(listing) for listing in processed_corpus]    
+    return dictionary, doc_term_matrix
+
+
+def LDA_topic_modelling(doc_term_matrix, dictionary, num_topics=3, passes=2):
+    # Create an object for LDA model and train it on Document-Term-Matrix
+    LDA = LdaModel
+    ldamodel = LDA(doc_term_matrix, num_topics=num_topics, id2word = dictionary, passes=passes)
+    return ldamodel
+
+def add_topics_to_df(ldamodel, doc_term_matrix, df, new_col, num_topics):
+    # Convert into Per-document topic probability matrix:
+    docTopicProbMat = ldamodel[doc_term_matrix]
+    docTopicProbDf = pd.DataFrame(index=df.index, columns=range(0, num_topics))
+    for i, doc in enumerate(docTopicProbMat):
+        for topic in doc:
+            docTopicProbDf.iloc[i, topic[0]] = topic[1]
+    docTopicProbDf[new_col] = docTopicProbDf.idxmax(axis=1)
+    df_topics = docTopicProbDf[new_col]
+    # Merge with df
+    df_new = pd.concat([df, df_topics], axis=1)
+    return df_new
+
+
+corpus.head()
+corpus_description = corpus.astype(str)
+processed_corpus_description = preprocess_text(corpus_description)
+dictionary_description, doc_term_matrix_description = nlp_model_pipeline(processed_corpus_description)
+
+ldamodel_description = LDA_topic_modelling(doc_term_matrix_description, dictionary_description, num_topics=2, passes=1)
+p = pyLDAvis.gensim.prepare(ldamodel_description, doc_term_matrix_description, dictionary_description)
+pyLDAvis.save_html(p, 'lda_2t.html')
+
+ldamodel_description = LDA_topic_modelling(doc_term_matrix_description, dictionary_description, num_topics=3, passes=1)
+p = pyLDAvis.gensim.prepare(ldamodel_description, doc_term_matrix_description, dictionary_description)
+pyLDAvis.save_html(p, 'lda_3t.html')
+
+ldamodel_description = LDA_topic_modelling(doc_term_matrix_description, dictionary_description, num_topics=4, passes=1)
+p = pyLDAvis.gensim.prepare(ldamodel_description, doc_term_matrix_description, dictionary_description)
+pyLDAvis.save_html(p, 'lda_4t.html')
+
+ldamodel_description = LDA_topic_modelling(doc_term_matrix_description, dictionary_description, num_topics=5, passes=1)
+p = pyLDAvis.gensim.prepare(ldamodel_description, doc_term_matrix_description, dictionary_description)
+pyLDAvis.save_html(p, 'lda_5t.html')
+
+ldamodel_description = LDA_topic_modelling(doc_term_matrix_description, dictionary_description, num_topics=6, passes=1)
+p = pyLDAvis.gensim.prepare(ldamodel_description, doc_term_matrix_description, dictionary_description)
+pyLDAvis.save_html(p, 'lda_6t.html')
+
+
+
+##############################################################################################################
+##############################################################################################################
+##############################################################################################################
+#modeling with scikit learn with Non-negative Matrix Factorization (NMF)
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.decomposition import NMF, LatentDirichletAllocation
+
+def display_topics(model, feature_names, no_top_words):
+    for topic_idx, topic in enumerate(model.components_):
+        print ("Topic %d:" % topic_idx)
+        print (" ".join([feature_names[i]
+                        for i in topic.argsort()[:-no_top_words - 1:-1]]))
+
+dataset = fetch_20newsgroups(shuffle=True, random_state=1, remove=('headers', 'footers', 'quotes'))
+documents = dataset.data
+
+no_features = 1000
+
+# NMF is able to use tf-idf
+tfidf_vectorizer = TfidfVectorizer(max_df=0.95, min_df=2, max_features=no_features, stop_words='english')
+tfidf = tfidf_vectorizer.fit_transform(corpus)
+tfidf_feature_names = tfidf_vectorizer.get_feature_names()
+
+# LDA can only use raw term counts for LDA because it is a probabilistic graphical model
+tf_vectorizer = CountVectorizer(max_df=0.95, min_df=2, max_features=no_features, stop_words='english')
+tf = tf_vectorizer.fit_transform(corpus)
+tf_feature_names = tf_vectorizer.get_feature_names()
+
+no_topics = 10
+
+# Run NMF
+nmf = NMF(n_components=no_topics, random_state=1, alpha=.1, l1_ratio=.5, init='nndsvd').fit(tfidf)
+
+# Run LDA
+lda = LatentDirichletAllocation(n_topics=no_topics, max_iter=5, learning_method='online', learning_offset=50.,random_state=0).fit(tf)
+
+no_top_words = 5
+display_topics(nmf, tfidf_feature_names, no_top_words)
+display_topics(lda, tf_feature_names, no_top_words)
+
+
+
